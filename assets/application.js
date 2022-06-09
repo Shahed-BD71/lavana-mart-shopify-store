f7a6b148ee38454bc20d3cf3d01f7811d3a70dc7
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-06-09 18:28:32 +0600

    add to cart fix

--- assets/application.js
+++ assets/application.js
@@ -6,3 +6,21 @@ if (document.getElementById('sort_by') != null) {
     window.location = url.href;
   });
 }
+
+// add to cart
+$(document).on('click','.ad_to_cart',function(){
+  var obj=$(this);
+ 
+  $.ajax({
+    type: 'POST',
+    url: '/cart/add.js',
+    data: {
+      quantity: 1,
+      id: $(this).attr("var_id")
+    },
+    dataType: 'json', 
+    success: function (data) {  
+          alert('Item added to cart');
+    } 
+  });
+})
